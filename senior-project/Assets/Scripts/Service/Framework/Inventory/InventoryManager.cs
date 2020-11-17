c4db0134e3efde2be0a9f3bce5b7f1839ec28a06
Aaron Hines <31013885+theRealTinyRick@users.noreply.github.com>
2020-11-16 19:55:13 -0500

    Moving files

---
+++ senior-project/Assets/Scripts/Service/Framework/Inventory/InventoryManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Service.Core.Framework
+{
+    public class InventoryManager : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
+
