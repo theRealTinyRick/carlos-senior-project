6fecce3ecb3fbf73692d282cfe7423ec85f45f41
Pkfire11 <71676898+Pkfire11@users.noreply.github.com>
2020-12-08 20:33:42 -0500

    carlos work

---
+++ MyFirstGame/Assets/Scripts/Gameplay/Door/Door.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private string OpenTrigger = "OpenDoor";
+    
+    [SerializeField]
+    private string CloseTrigger = "CloseDoor";
+
+    private bool isOpen = false;
+
+    void Start()
+    {
+        if(animator == null)        
+        {
+            animator = GetComponent<Animator>();
+            if(animator == null)
+            {
+                Debug.LogError("No animator found.");
+            }
+        }
+    }
+
+    public void Open()
+    {
+        if(isOpen) return;
+
+        if(animator != null)
+        {
+            animator.SetTrigger(OpenTrigger);
+            isOpen = true;
+        }
+    }   
+
+    public void Close()
+    {
+        if(!isOpen) return;
+
+        if(animator != null)
+        {
+            animator.SetTrigger(CloseTrigger);
+            isOpen = false;
+        }
+    }
+}
--- MyFirstGame/Assets/Scripts/movement.cs
+++ MyFirstGame/Assets/Scripts/movement.cs
@@ -3,43 +3,43 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
- 
-    public Rigidbody rb;  
+    [SerializeField]
+    private Camera cam;
 
-    public float forwardForce = 1000f;
-    public float sideForce = 750f;
+    public Rigidbody rb;
+
+    public float moveSpeed = 5;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            cam = FindObjectOfType<Camera>();
+            if (cam == null)
+            {
+                Debug.LogError("No camera found.");
+            }
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
-    {  
-        if(Input.GetKey("a"))
-        {
-            rb.AddForce(0,0,forwardForce * Time.deltaTime);
-        }
-        
-        if(Input.GetKey("d"))
-        {
-           rb.AddForce(0,0,-forwardForce * Time.deltaTime); 
-        }
+    {
+        Vector3 _moveDir = new Vector3();
+        _moveDir = new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, rb.velocity.y,Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
 
-        if(Input.GetKey("w")) {
-            rb.AddForce(sideForce * Time.deltaTime,0,0);
-        
-        }
-        if(Input.GetKey("s")) 
-        {
-            rb.AddForce(-sideForce * Time.deltaTime,0,0);
-        }    
+        rb.velocity = _moveDir; 
 
         if (Input.GetMouseButtonDown(0))
         {
-            //Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            //RaycastHit hit; 
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-            //if (Physics.Raycast(ray, out hit, 100)) 
-            //{
-            //    Interactables interactable1 = hit.collider.GetComponent<Interactables>();
-                    
-           // }
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactables interactable1 = hit.collider.GetComponent<Interactables>();
+                Debug.Log("Hit something.");
+            }
         }
     }
 }
